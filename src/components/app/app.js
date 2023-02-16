5be6dc5864d2347730dc397a263c79af5f6c7f7b
Dolganov_830501 <dolganovvlad486@gmail.com>
2023-02-16 23:20:32 +0300

    redact

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,16 +1,27 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
-import './app.css';
+import AppFilter from '../app-filter/app-filter';
+import EmployersList from '../employers-list/employers-list';
 
+import './app.css';
 function App(){
+
+   const data = [
+      {name: 'Alex F.D' , salary : 1200, increase:false },
+      {name: "Johan Y.K" , salary : 600, increase:true },
+      {name: "Frenk H.S" , salary : 900,increase:false },
+   ];
+
    return(
       <div className="app">
          <AppInfo/>
 
          <div className='search-panel'>
-            <SearchPanel/>
+               <SearchPanel/>
+               <AppFilter/>
          </div>
 
+         <EmployersList data = {data}/>
       </div>
    );
 }
---
+++ src/components/employers-list/employers-list.js
@@ -0,0 +1,20 @@
+import EmployersListItem from "../employers-list-item/employers-list-item";
+
+import "./employera-list.css"
+
+const EmployersList = ({data}) =>{
+   
+   const elements = data.map(item =>{
+      return(
+         <EmployersListItem {...item}/> //
+      )
+   })
+   
+   return(
+      <ul className="app-list list-group">
+         {elements}
+      </ul>
+   ); 
+}
+
+export default EmployersList;
