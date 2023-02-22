9bfbac9d13f8d49a23d2041f52cb3970c2be708e
Dolganov_830501 <dolganovvlad486@gmail.com>
2023-02-22 23:51:33 +0300

    delete

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,29 +1,55 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployersList from '../employers-list/employers-list';
 
 import './app.css';
-function App(){
+ 
+class App extends Component{
+   constructor(props){
+      super(props);
+      this.state = {
+         data : [
+            {name: 'Alex F.D' , salary : 1200, increase:false, id:1 },
+            {name: "Johan Y.K" , salary : 600, increase:true, id:2 },
+            {name: "Frenk H.S" , salary : 900,increase:false, id:3 },
+         ]
+      }
+   }
+   
+   deleteItem = (id) => {
+      this.setState(({data}) =>{
 
-   const data = [
-      {name: 'Alex F.D' , salary : 1200, increase:false, id:1 },
-      {name: "Johan Y.K" , salary : 600, increase:true, id:2 },
-      {name: "Frenk H.S" , salary : 900,increase:false, id:3 },
-   ];
+         // const index = data.findIndex(elem =>elem.id === id);
+         // const before = data.slice(0, index);
+         // const after = data.slice(index+1);
+         // const newArr = [...before, ...after];
 
-   return(
-      <div className="app">
-         <AppInfo/>
+         return{
+            // data : newArr
+            data : data.filter(item => item.id !== id)
+         }
+      })
+   }
 
-         <div className='search-panel'>
-               <SearchPanel/>
-               <AppFilter/>
+   render(){
+      return(
+         <div className="app">
+            <AppInfo/>
+   
+            <div className='search-panel'>
+                  <SearchPanel/>
+                  <AppFilter/>
+            </div>
+   
+            <EmployersList 
+               data = {this.state.data}
+               onDelete = {this.deleteItem}/>
          </div>
-
-         <EmployersList data = {data}/>
-      </div>
-   );
+      );
+   }
 }
 
 export default App;
